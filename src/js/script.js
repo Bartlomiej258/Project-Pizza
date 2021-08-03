89bcb187b160a832b937bdfd636c94d1790777da
Adrian Sadowski <sadow.adrian@gmail.com>
2021-08-03 19:37:34 +0200

    instant for all products

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,17 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log ('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data)
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
     },
 
     init: function(){
@@ -73,10 +82,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-      
+
+      thisApp.initData();
       thisApp.initMenu();
     },
-
-  }
+  };
   app.init();
 }
