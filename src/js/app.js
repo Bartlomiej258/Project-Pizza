251be599b4219a61d403387641015f5aac4042bb
Adrian Sadowski <sadow.adrian@gmail.com>
2021-08-23 17:16:08 +0200

    add class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
@@ -88,19 +89,6 @@ const app = {
       });
   },
 
-  init: function(){
-    const thisApp = this;
-    //console.log('*** App starting ***');
-    //console.log('thisApp:', thisApp);
-    //console.log('classNames:', classNames);
-    //console.log('settings:', settings);
-    //console.log('templates:', templates);
-    thisApp.initPages();
-    thisApp.initData();
-    thisApp.initMenu();
-    thisApp.initCart();
-    //console.log('this.initCart:', thisApp.cart);
-  },
   initCart: function() {
     const thisApp = this;
     const cartElem = document.querySelector(select.containerOf.cart);
@@ -112,6 +100,30 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+
+  initBooking: function() {
+    const thisApp = this;
+
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWidget);
+
+  },
+
+  init: function(){
+    const thisApp = this;
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initMenu();
+    thisApp.initCart();
+    thisApp.initBooking();
+    //console.log('this.initCart:', thisApp.cart);
+  },
+
 };
 
 app.init();
---
+++ src/js/components/Booking.js
@@ -0,0 +1,37 @@
+import { select, templates } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+    //debugger;
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.innerHTML = generatedHTML;
+    //console.log('thisBooking.dom.wrapper.innerHTML', thisBooking.dom.wrapper.innerHTML)
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+    
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+    thisBooking.dom.peopleAmount.addEventListener('updated', function() {});
+    thisBooking.dom.hoursAmount.addEventListener('updated', function() {});
+
+  }
+
+}
+export default Booking;
